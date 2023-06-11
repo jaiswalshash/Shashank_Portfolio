d3314650f4be54c6b780dd46b8dd08f548027b41
Shashank Jaiswal <jaiswalshashank123@gmail.com>
2023-06-11 23:15:37 +0530

    Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,7 @@ const Navbar = () => {
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
-      style={{
-        paddingLeft: "0",
-        paddingRight: "0"
-      }}
+
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
@@ -28,7 +25,7 @@ const Navbar = () => {
         >
           <img src = {logo} alt = "logo" className='flex w-15 h-10 object-contain' />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Shashank Jaiswal <span className='sm:block hidden'> | Web Developer</span>
+            Shashank &nbsp;<span className='sm:block hidden'>| Web Developer</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
