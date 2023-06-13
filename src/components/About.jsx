317d2b6f572fb03d70df0726dc82702d65a52011
Shashank Jaiswal <jaiswalshashank123@gmail.com>
2023-06-13 22:12:10 +0530

    Updated About section and Higher Order  Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+import { SectionWrapper } from '../hoc';
 
-const About = () => {
+import {styles} from "../style";
+import {services} from "../constants";
+import {fadeIn, textVariant} from "../utils/motion";
+
+const   About = () => {
+
+  const ServiceCard = ({index, title, icon}) => {
+    return(
+      <Tilt className ="xs:w-[250px]  w-full">
+        <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        >
+          <div
+            options = {{
+              max:45,
+              scale:  1,
+              speed:450
+            }}
+            className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+          >
+            <img src= {icon} alt={title} className='w-16 h-16 object-contain' />
+            <h3 className='text-white font-bold text-center text-[20px]'>{title}</h3>
+          </div>
+        </motion.div>
+      </Tilt>
+    )
+  }
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+       className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+      Lorem ipsum dolor sit amet, consectetur 
+      adipiscing elit. Etiam nec lorem ultrices, 
+      imperdiet nisl ut, laoreet lorem. Fusce consequat 
+      ipsum ut enim vehicula viverra. Cras id porttitor 
+      libero. Suspendisse aliquet risus eget orci iaculis, 
+      sit amet blandit ipsum feugiat. In non felis massa.
+       Praesent rutrum eu ex quis iaculis. Quisque tincidunt e
+       st libero, non rutrum enim auctor ac. Nam eu libero sit 
+       amet massa semper vestibulum non eu dolor. Fusce tempor 
+       purus augue, sed sollicitudin urna tempus imperdiet. 
+       Aliquam metus sem, laoreet vitae rhoncus id, hendrerit 
+       vitae tellus. In id eleifend diam. Nulla vitae consectetur 
+       leo. Morbi viverra cursus massa, id iaculis eros condimentum in.
+
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => {
+          return(
+            <ServiceCard key={service.title} index={index} {...service}/>
+          );
+        })
+
+        }
+      </div>
+    </>
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,15 @@
-import {Suspense, useState, useEffect } from 'react'
-import { Canvas } from '@react-three/fiber'; 
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from "../Loader"
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor= 'black' />
-      <pointLight intensity={1}/>
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,36 +18,61 @@ const Computers = () => {
         castShadow
         shadow-mapSize={1024}
       />
-      <primitive 
+      <pointLight intensity={1} />
+      <primitive
         object={computer.scene}
-        scale= {0.75}
-        position= {[0, -3.25, -1.5]}
-        rotation = {[-0.01, -0.2, -0.01]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
-    return (
-      <Canvas
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
       frameloop='demand'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-        <Suspense fallback= {<CanvasLoader /> }>
-          <OrbitControls 
-          enableZoom = {false}
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-          />
-          <Computers />
-        </Suspense>
-        <Preload all />
-      </Canvas>
-    )
-}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
 
 export default ComputersCanvas;
